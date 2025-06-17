99f5af144f514d6bfc28f41b4302e1fb3cbc5c7f
YeongJae0114 <yjl8628@naver.com>
2025-06-17 23:16:13 +0900

    🔧 Chore: 기본 템플릿 파일 제거 및 프로젝트 구조 초기화
    - app.controller.ts, app.service.ts 등 Nest 기본 파일 제거

--- first-project/src/app.module.ts
+++ first-project/src/app.module.ts
@@ -1,10 +1,33 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
+import { APP_GUARD } from '@nestjs/core';
+import { SessionGuard } from './auth/guards/session.guard';
+import { PostModule } from './posts/posts.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'dudwo',
+      database: 'test_db',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true, // 개발용: 자동 마이그레이션 (운영에서는 false)
+      dropSchema: true,
+    }),
+    UserModule,
+    AuthModule,
+    PostModule,
+  ],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: SessionGuard,
+    },
+  ],
 })
 export class AppModule {}
